657eed5cfcce054dabfe1bb9311fce8c968e37a0
Owen McGill <owenedward.mcgill@edu.hefr.ch>
2018-02-22 21:30:51 +0100

    Change file icon size to 16x16, so as to display correctly

--- src/ch/eif/intelliprolog/PrologIcons.java
+++ src/ch/eif/intelliprolog/PrologIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class PrologIcons {
-    public static final Icon FILE = IconLoader.getIcon("/ch/eif/intelliprolog/icons/log.png");
+    public static final Icon FILE = IconLoader.getIcon("/ch/eif/intelliprolog/icons/logo.png");
 }
