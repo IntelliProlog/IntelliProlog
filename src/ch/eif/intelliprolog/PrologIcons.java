f142d07d4e1da7a82931a3e877a1a67630959f8d
Owen McGill <owenedward.mcgill@edu.hefr.ch>
2018-02-22 11:36:12 +0100

    Add GNU Prolog icon and class for loading it PrologIcons

---
+++ src/ch/eif/intelliprolog/PrologIcons.java
@@ -0,0 +1,9 @@
+package ch.eif.intelliprolog;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class PrologIcons {
+    public static final Icon FILE = IconLoader.getIcon("/ch/eif/intelliprolog/icons/log.png");
+}
