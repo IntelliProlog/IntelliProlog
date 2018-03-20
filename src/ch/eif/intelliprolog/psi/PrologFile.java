4033ff3dd6ec1508a63112760b839d942db54c86
Owen McGill <owenedward.mcgill@edu.hefr.ch>
2018-03-20 11:14:24 +0100

    Add PrologFile as PsiFileBase, used for the moment for checking if selected file for run configuration is a PrologFile, uses PrologFileType

---
+++ src/ch/eif/intelliprolog/psi/PrologFile.java
@@ -0,0 +1,26 @@
+package ch.eif.intelliprolog.psi;
+
+import ch.eif.intelliprolog.PrologFileType;
+import ch.eif.intelliprolog.PrologLanguage;
+import com.intellij.extapi.psi.PsiFileBase;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.psi.FileViewProvider;
+import org.jetbrains.annotations.NotNull;
+
+public class PrologFile extends PsiFileBase {
+    protected PrologFile(@NotNull FileViewProvider viewProvider) {
+        super(viewProvider, PrologLanguage.INSTANCE);
+    }
+
+    @NotNull
+    @Override
+    public FileType getFileType() {
+        return PrologFileType.INSTANCE;
+    }
+
+    @Override
+    public String toString() {
+        return "Prolog File";
+    }
+
+}
