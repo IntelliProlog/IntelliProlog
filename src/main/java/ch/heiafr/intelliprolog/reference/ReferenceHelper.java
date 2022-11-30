44a2eda3b3c2ba80337a8d7dc5955be517837195
Erwan Sturzenegger <erwan@sturzy.ch>
2022-11-30 10:14:50 +0100

    Modification du calcul de l'arité

--- src/main/java/ch/heiafr/intelliprolog/reference/ReferenceHelper.java
+++ src/main/java/ch/heiafr/intelliprolog/reference/ReferenceHelper.java
@@ -2,10 +2,8 @@ package ch.heiafr.intelliprolog.reference;
 
 import ch.heiafr.intelliprolog.psi.*;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.psi.util.PsiTreeUtil;
-import org.apache.lucene.index.Term;
-
-import java.util.Objects;
 
 public class ReferenceHelper {
 
@@ -45,6 +43,12 @@ public class ReferenceHelper {
         return null;
     }
 
+    /**
+     * Check if a pattern fit a PsiElement
+     * @param elt The element to check
+     * @param pattern The pattern to check
+     * @return The element if the pattern fit, null otherwise
+     */
     private static PsiElement patternFitPsiElement(PsiElement elt, Class[] pattern) {
         for (Class<?> aClass : pattern) {
             if (!aClass.isInstance(elt)) {
@@ -77,7 +81,14 @@ public class ReferenceHelper {
     }
 
 
+    /**
+     * Calculate the arity of a compound
+     * @param compound The compound
+     * @return The arity of the compound, 0 if this is not a compound
+     */
     public static int getArity(PsiElement compound) {
+
+        //TODO: ERROR when predicate is predicate(A, B, C - D) => arity = 4 instead of 3
 
         if (!(compound instanceof PrologCompound)) {
             return 0;
@@ -89,14 +100,58 @@ public class ReferenceHelper {
 
         int count = 0;
 
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Arity of " + compound.getText());
+
         while (term != null) {
             count++;
+            term = skipComposed(term);
+            System.out.println("End of skipComposed: " + term.getText());
             term = applyIfPossible(term, singleParameterPattern);
         }
 
+        System.out.printf("Arity of %s is %d%n", compound.getText(), count);
+        System.out.println("------------------------------------------------------------");
         return count;
     }
 
+    /**
+     * Find last element of a composed term.
+     * Example: A-B-C-D, E => D, E
+     * @param term The term to search in
+     * @return The last element of the composed term
+     */
+    private static PsiElement skipComposed(PsiElement term) {
+        Class<?>[][] findOperator = new Class<?>[][]{{PrologTerm.class}, {PrologOperation.class}, {PrologNativeBinaryOperation.class},{PrologBasicTerm.class, PrologKnownBinaryOperator.class}};
+        PsiElement initial = term;
+
+        System.out.println("Skip composed: " + term.getText());
+
+        var op = applyIfPossible(term, findOperator);
+        if(op == null){
+            return initial;
+        }
+
+        if (op.getText().equals(",")) {
+            return initial;
+        } else {
+            //Find next term and apply
+            term = op.getNextSibling();
+            while (term instanceof PsiWhiteSpace) {
+                //Skip whitespaces
+                term = term.getNextSibling();
+            }
+
+            return skipComposed(term);
+        }
+    }
+
+    /**
+     * Apply a pattern to a PsiElement to find the next element
+     * @param term The element to search in
+     * @param pattern The pattern to apply
+     * @return The next element if the pattern was found, null otherwise
+     */
     private static PsiElement applyIfPossible(PsiElement term, Class<?>[][] pattern) {
         if (term == null) {
             return null;
@@ -110,6 +165,9 @@ public class ReferenceHelper {
                     continue;
                 }
                 term = term.getNextSibling();
+                while(term instanceof PsiWhiteSpace){
+                    term = term.getNextSibling(); //Skip whitespaces
+                }
                 if (term == null) {
                     return null;
                 }
@@ -122,14 +180,24 @@ public class ReferenceHelper {
         return term;
     }
 
+    /**
+     * Find a compound from a PSI element
+     * @param clickedElement The element to search in
+     * @return The compound if found, null otherwise
+     */
     public static PrologCompound compoundFromClickedElement(PsiElement clickedElement) {
-        if(clickedElement.getNode().getElementType() == PrologTypes.UNQUOTED_COMPOUND_NAME
-        || clickedElement instanceof PrologCompoundName){
+        if (clickedElement.getNode().getElementType() == PrologTypes.UNQUOTED_COMPOUND_NAME
+                || clickedElement instanceof PrologCompoundName) {
             return PsiTreeUtil.getParentOfType(clickedElement, PrologCompound.class);
         }
         return null;
     }
 
+    /**
+     * Find the Compound name from a Compound
+     * @param compound The compound to search in
+     * @return The compound name if found, null otherwise
+     */
     public static String compoundNameFromCompound(PrologCompound compound) {
         PrologCompoundName compoundName = PsiTreeUtil.findChildOfType(compound, PrologCompoundName.class);
         if (compoundName == null) {
@@ -138,6 +206,11 @@ public class ReferenceHelper {
         return compoundName.getText();
     }
 
+    /**
+     * Find the arity of a PSI element
+     * @param elt The element to search in
+     * @return The arity if found, 0 otherwise
+     */
     public static int getArityFromClicked(PsiElement elt) {
         return getArity(compoundFromClickedElement(elt));
     }
