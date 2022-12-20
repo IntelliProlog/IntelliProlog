2fded9665c814904e207c38070dcff39b1b14190
Erwan Sturzenegger <erwan@sturzy.ch>
2022-12-20 14:45:37 +0100

    Update pour la nouvelle version d'IntelliJ Idea

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,6 +77,7 @@ tasks {
     patchPluginXml {
         version.set(properties("pluginVersion"))
         sinceBuild.set(properties("pluginSinceBuild"))
+        //untilBuild.set(properties("pluginUntilBuild"))
     }
 
 
